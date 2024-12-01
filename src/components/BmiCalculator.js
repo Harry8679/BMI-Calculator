fa85993e177433296e7b411d8841f32c5a87825e
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-01 05:45:14 +0100

    On Select Tag

--- src/components/BmiCalculator.js
+++ src/components/BmiCalculator.js
@@ -1,23 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FormInput from './FormInput';
 
 const BmiCalculator = () => {
+  const [heightUnit, setHeightUnit] = useState('');
+  const [weightUnit, setWeightUnit] = useState('');
+  const [unit, setUnit] = useState('');
+
   const onChangeInput = (e) => {};
+  const onSelectTag = (e) => {
+    setUnit(e.target.value);
+    if (e.target.value === 'Metric') {
+        setHeightUnit('cm');
+        setWeightUnit('kg');
+    } else {
+        setHeightUnit('ft');
+        setWeightUnit('lbs');
+    };
+  }
+
   return (
     <div className='bmi-inputs'>
       <div className='input-fields'>
         <div>
             <span className='label-unit'>Unit</span>
             <div className='unit'>
-                <select name="unit" value='' className='form-control form-control-sm'>
+                <select name="unit" value={unit} className='form-control form-control-sm' onChange={onSelectTag}>
                     <option value="Metric">Metric</option>
                     <option value="Imperial">Imperial</option>
                 </select>
             </div>
         </div>
-        <FormInput type='text' name='heightCount' title={`Height (cm)`} value='' onChange={onChangeInput} />
-        <FormInput type='text' name='inchesCount' title={` (in)`} value='' onChange={onChangeInput} />
-        <FormInput type='text' name='weightCount' title={`Weight (kg)`} value='' onChange={onChangeInput} />
+        <FormInput type='text' name='heightCount' title={`Height (${heightUnit})`} value='' onChange={onChangeInput} />
+        {unit === 'Imperial' ? <FormInput type='text' name='inchesCount' title={` (in)`} value='' onChange={onChangeInput} /> : ''}
+        <FormInput type='text' name='weightCount' title={`Weight (${weightUnit})`} value='' onChange={onChangeInput} />
       </div>
 
       <button className='button' type='submit'>Reset</button>
@@ -25,4 +40,4 @@ const BmiCalculator = () => {
   )
 }
 
-export default BmiCalculator
+export default BmiCalculator;
