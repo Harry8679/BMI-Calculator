576c459082049aa89bfc43ea07571b3f50120500
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-01 04:10:23 +0100

    Bmi Calculator updated

--- src/components/BmiCalculator.js
+++ src/components/BmiCalculator.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 const BmiCalculator = () => {
   return (
-    <div>
-      Bmi Calculator
+    <div className='bmi-inputs'>
+      <div className='input-fields'>
+        <div>
+            <span className='label-unit'>Unit</span>
+            <div className='unit'>
+                <select name="unit" value='' className='form-control form-control-sm'>
+                    <option value="Metric">Metric</option>
+                    <option value="Imperial">Imperial</option>
+                </select>
+            </div>
+        </div>
+        <input type="text" />
+      </div>
+
+      <button className='button' type='submit'>Reset</button>
     </div>
   )
 }
