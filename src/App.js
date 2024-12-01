253e7d3936f1ed2bd67412ac607c3887e4f49d40
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-01 04:17:28 +0100

    Container class added in App Component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import BmiCalculator from './components/BMI';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <div className='row justify-content-center'>
         <div className='col-lg-6 col-lg-offset'>
           <BmiCalculator />
