1fce6716911af4fa28afe474b9e960226d06905f
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-01 06:02:28 +0100

    Values By Default

--- src/components/BmiCalculator.js
+++ src/components/BmiCalculator.js
@@ -5,6 +5,13 @@ const BmiCalculator = () => {
   const [heightUnit, setHeightUnit] = useState('cm');
   const [weightUnit, setWeightUnit] = useState('kg');
   const [unit, setUnit] = useState('');
+  const [count, setCount] = useState({
+    heightCount: '0',
+    inchesCount: '0',
+    weightCount: '0',
+  });
+
+  const { heightCount, inchesCount, weightCount } = count;
 
 //   useEffect(() => {
 //     if (unit === '') {
@@ -38,9 +45,9 @@ const BmiCalculator = () => {
                 </select>
             </div>
         </div>
-        <FormInput type='text' name='heightCount' title={`Height (${heightUnit})`} value='' onChange={onChangeInput} />
-        {unit === 'Imperial' ? <FormInput type='text' name='inchesCount' title={` (in)`} value='' onChange={onChangeInput} /> : ''}
-        <FormInput type='text' name='weightCount' title={`Weight (${weightUnit})`} value='' onChange={onChangeInput} />
+        <FormInput type='text' name='heightCount' title={`Height (${heightUnit})`} value={heightCount} onChange={onChangeInput} />
+        {unit === 'Imperial' ? <FormInput type='text' name='inchesCount' title={` (in)`} value={inchesCount} onChange={onChangeInput} /> : ''}
+        <FormInput type='text' name='weightCount' title={`Weight (${weightUnit})`} value={weightCount} onChange={onChangeInput} />
       </div>
 
       <button className='button' type='submit'>Reset</button>
