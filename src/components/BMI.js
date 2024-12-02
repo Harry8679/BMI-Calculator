de588a828c64a08c27013abbf065f989e33a3182
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-02 07:27:59 +0100

    Get BMI Class

--- src/components/BMI.js
+++ src/components/BMI.js
@@ -3,6 +3,13 @@ import BmiCalculator from './BmiCalculator';
 
 const BMI = () => {
   const [bmiValue, setBmiValue] = useState(0);
+
+  const getBmiClass = bmi => {
+    if (bmi >= 1 && bmi <= 18.5) return 'Underweight';
+    if (bmi >= 18.5 && bmi <= 24.9) return 'Normal weight';
+    if (bmi >= 24.9 && bmi <= 29.9) return 'Overweight';
+    if (bmi >= 30) return 'Obese';
+  }
   return (
     <>
       <div className='calculator'>
